51603360c813f1d69878311d6790bd4a8325b8dc
Evan <68429595+ConsciousBone@users.noreply.github.com>
2025-10-21 18:31:46 +0100

    Initial Commit

---
+++ Leafy/LeafyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeafyApp.swift
+//  Leafy
+//
+//  Created by Evan Plant on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LeafyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
